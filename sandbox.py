96ff774feb5b0609972f3132babafc66ad1804c2
Mayra Silva <mayraeduarda2002@gmail.com>
2025-04-01 16:24:05 -0300

    teste

--- sandbox.py
+++ sandbox.py
@@ -23,8 +23,28 @@ def ftype(v):
     return type(v)
 
 
-txt = "welcome to the jungle"
+# txt = "welcome to the jungle"
+# x = txt.split()
+# print(x)
 
-x = txt.split()
 
-print(x)
+
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+print(thisdict["brand"])
+
+dict1 = {'126': 'felicidade', 'b': 2}
+dict2 = {'felicidade': '126', 'y': 3}
+
+# Check if any value in dict1 is present as a value in dict2
+for key, value in dict1.items():
+    if key in dict2.values():
+        print(f"The value {value} from dict1 is in dict2.")
+
+
+dict1 = {'126': 'positive', 'location': 'city'}
+dict2 = {'amora': ['Alice', '126'], 'age': 23}
+
+# Check if any key in dict2 is present as a value in dict1
+for key in dict2:
+    if key in dict1.values():
+        print(f"The key '{key}' from dict2 is a value in dict1.")
